55cb70812e952894fa1c10e7bf28e4c2b19ce566
Jose Armando Lopez Francisco <fcoarmando1823@gmail.com>
2026-02-09 13:42:50 -0600

    Add files via upload

--- t2-ej3.py
+++ t2-ej3.py
@@ -1,10 +1,7 @@
 import math as mt
-print("="*70)
+print("="*63)
 print(f"| {'n':^5} | {'Aproximacion':^15} | {'Error abs':^15} | {'Valor real':^15} |")
-print("="*70)
-
-x = 0.5
-val_real = mt.exp(x)
+print("="*63)
 
 def taylor_exp(x, tole=1e-3):
     sum = 0
@@ -14,14 +11,20 @@ def taylor_exp(x, tole=1e-3):
         sum += term
         k += 1
         term = x**k/mt.factorial(k)
-    return sum, k
+        error = abs(val_real - sum)
+        print(f"| {k:^5} | {sum:^15.8f} | {error:^15.8f} | {val_real:^15.8f} |")
+    return sum, k, error
 
-def taylor_exp1(k, n):
-    sum = 0
-    tol = 1e-3
-    for k in range(n+1):
-        term = sum + x**k/mt.factorial(k)
-        sum += term
-        if term >= tol:
-            print(f"Aproximacion encontrada en n = {k+1}")
-            break
+x = 0.5
+val_real = mt.exp(x)
+tol = 1e-3
+
+aprox, num_i, error_abs = taylor_exp(x, tol)
+
+print("="*63)
+print("\n")
+print("-"*50)
+print(f"Valor real: {val_real}")
+print(f"Aproximacion: {aprox}")
+print(f"Aproximacion encontrada en: {num_i}")
+print("-"*50)
