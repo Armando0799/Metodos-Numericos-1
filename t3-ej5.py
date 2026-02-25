ba178ebff1f2b77f2df3714541a03847f9e03a57
Armando0799 <fcoarmando1823@gmail.com>
2026-02-24 19:51:55 -0600

    aprox de sqrt 2 con met biseccion

---
+++ t3-ej5.py
@@ -0,0 +1,38 @@
+import math
+def f(x):
+    return x**2-2
+
+a = 1
+b = 2
+iter = int(input("Ingrese el numero de iteraciones a realizar: "))
+f_a = f(a)
+f_b = f(b)
+print("="*98)
+print(f"| {'n':^4} | {'a':^15} | {'b':^15} | {'p0':^15} | {'f(p_a)*f(p_0)':^15} | {'Eval en f(x)':^15} |")
+print("="*98)
+
+if f_a * f_b < 0:
+    p0 = 0
+    f_p0 = 0
+    f_pa = 0
+    for n in range(iter):
+        p0 = (a + b)/2
+        f_p0 = f(p0)
+        f_pa = f(a)
+        mult = f_pa * f_p0
+        print(f"| {n+1:^4} | {a:^15.8f} | {b:^15.8f} | {p0:^15.8f} | {mult:^15.8f} | {f_p0:^15.8f} |")
+
+        if f_p0 == 0:
+            print(f"| {1:^4} | {a:^15.8f} | {b:^15.8f} | {p0:^15.8f} | {mult:^15.8f} | {f_p0:^15.8f} |")
+            break
+        if f(a) * f_p0 < 0:
+            b = p0
+        else:
+            a = p0
+    print("="*98)
+    print(f"\n Raiz encontrada en {p0} \n")
+    print(f"Evaluacion de la raiz en la funcion f(x): {f_p0} \n")
+else:
+    print("="*60)
+    print("ERROR: La funcion debe de tener signos opuestos al evaluar en los extremos del intervalo.")
+    print("="*60)
